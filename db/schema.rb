f5cc5a58029763657e498c6dab732a549b5d929d
Saravana <saran@agiratech.com>
2016-12-31 22:26:00 +0530

    Schema file modification

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161231072618) do
+ActiveRecord::Schema.define(version: 20161231142416) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "contacts", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.string   "phone"
+    t.text     "address"
+    t.date     "birthday"
+    t.string   "circle"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_contacts_on_user_id", unique: true, using: :btree
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
