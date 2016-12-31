ccce5d1d4cd206b2c79575395f67132251408eb5
Saravana <saran@agiratech.com>
2016-12-31 15:49:54 +0530

    Rubocop code styles added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
+# frozen_string_literal: true
 class User < ApplicationRecord
-
   # authenticate against a BCrypt password
   has_secure_password
   # token authentication
@@ -8,4 +8,13 @@ class User < ApplicationRecord
   # model validations
   validates :first_name, presence: true
   validates :email, presence: true, uniqueness: true, case_sensitive: false
+
+  # To form json data
+  def to_json
+    {
+      first_name: first_name,
+      last_name: last_name,
+      email: email
+    }
+  end
 end
